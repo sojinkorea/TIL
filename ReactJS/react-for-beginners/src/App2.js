eb31e690401c59b883b00b2f82f5ca2a15388bd7
sojinjang <wkdthwls16@naver.com>
2023-10-17 01:07:24 +0900

    📝 2023.10.17

--- ReactJS/react-for-beginners/src/App2.js
+++ ReactJS/react-for-beginners/src/App2.js
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("render");
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("i run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
--- ReactJS/react-for-beginners/src/index.js
+++ ReactJS/react-for-beginners/src/index.js
@@ -4,8 +4,4 @@ import App from "./App";
 import App2 from "./App2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App2 />
-  </React.StrictMode>
-);
+root.render(<App2 />);
