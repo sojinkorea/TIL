47bf915433e6856c19672ff6e50fd448d26059bc
sojinjang <wkdthwls16@naver.com>
2023-10-17 23:44:31 +0900

    📝 CleanupFn

---
+++ ReactJS/react-for-beginners/src/App3.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  function byeFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
+function App3() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App3;
--- ReactJS/react-for-beginners/src/index.js
+++ ReactJS/react-for-beginners/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import App2 from "./App2";
+// import App from "./App";
+// import App2 from "./App2";
+import App3 from "./App3";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App2 />);
+root.render(<App3 />);
